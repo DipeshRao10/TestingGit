f6059130f840456337f70fcbbe1daeb41cf22412
DipeshRao10 <103821191+DipeshRao10@users.noreply.github.com>
2022-04-19 11:59:37 +0530

    commit1

---
+++ GithubTest/src/test/loginPage.java
@@ -0,0 +1,9 @@
+package test;
+
+public class loginPage {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ GithubTest/src/test/tetinghome.java
@@ -0,0 +1,9 @@
+package test;
+
+public class tetinghome {
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
